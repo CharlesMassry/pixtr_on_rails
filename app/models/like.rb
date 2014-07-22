b8ab83413447e3ffd2a6c2b99fa95ab5977502d2
Charlie Massry <charlesdmassry@gmail.com>
2014-07-22 14:37:39 -0400

    Cleaned up custom like validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,14 +1,12 @@
 class Like < ActiveRecord::Base
   validates :user_id, uniqueness: { scope: [:content_id, :content_type]  }
-  validate :user_cant_like_himself
+  validate :user_cannot_like_self
   belongs_to :user
   belongs_to :content, polymorphic: true
 
-  def user_cant_like_himself
-    if self.content_type == "User"
-      if self.content_id == self.user_id
+  def user_cannot_like_self
+    if content == user
         errors.add(:user_id, "You can't like yourself")
-      end
     end
   end
 
